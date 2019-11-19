8426a112bf5cd8c7ced0c00161b8ae6e0ee06269
akarsh1888 <akarsh1888@gmail.com>
2019-11-20 01:48:59 +0530

    Added Routing Libraries ie.react-router-dom , Link, Router, withRouter Added linkUrl properties to the array of objects Now [MenuItem] is configured to receive [linkUrl] & append it to the current url & push it Each MenuItem has routing capability based on the [LinkUrl prop]

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
+
+// Providing routing object ie.[match object] functionality
+import { withRouter } from 'react-router-dom';
+
 // Styles
 import './menu-item.styles.scss';
 
 
-const MenuItem = ({title, imageUrl, size}) => {
+const MenuItem = ({title, imageUrl, size, linkUrl, history, match}) => {
   return (
-    <div className={`${size} menu-item`}>
-      
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
       {/* We are disconnecting the backgroundimage with the [content] so it can't move [content] on hover*/}
-        <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
-        
-            <div className="content">
-          <h1 className="title">{title.toUpperCase()}</h1>
-          <span className="subtitle">SHOP NOW</span>
-            </div>
-            
-      </div>
-    );
-};
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
 
-export default MenuItem;
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+ 
+export default withRouter(MenuItem);
