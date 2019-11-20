4b8d843e3a6cd12b56a3b9900ebba97c6f6fd153
akarsh1888 <akarsh1888@gmail.com>
2019-11-21 01:15:49 +0530

    Created 1.SignUp Component + 2.Coding for signin button SignUp==Creates/checks username info in db + signed in the user also signin button===on clicking it sign in the user by checking [user authentication] data from db

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -7,7 +7,7 @@ import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
 //Signinwithgoogle function from firebase utils
-import { signInWithGoogle  } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle  } from "../../firebase/firebase.utils";
 
 class SignIn extends Component {
   constructor() {
@@ -24,12 +24,29 @@ class SignIn extends Component {
     
     
     
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
+    
+    const { email, password } = this.state;
 
-    this.setState({ email: "", password: "" });
-  };
+    try
+    {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    }
+    catch (error)
+    {
+      console.log(error);
+    }
 
+     };
+
+  
+  
+  
+  
+  
+  
   
     
     handleChange = event => {
